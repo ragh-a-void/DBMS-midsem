a8361c6febed70e0fb7acc57e271d4e8ba03bf95
prashasti19075 <55681151+prashasti19075@users.noreply.github.com>
2021-03-10 16:46:08 +0530

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,4 +1,15 @@
+-- CREATE VIEW PanelistView 
+-- AS 
+-- SELECT * FROM panellist, submission
+-- WHERE panellist.CandidateNo=convert(left(CURRENT_USER(),INSTR(current_user(), '@')-1), decimal);
+
+create user "30001"@localhost identified by "12345";
+
+select substring_index(user(), '@', 1);
 CREATE VIEW PanelistView 
 AS 
 SELECT * FROM panellist, submission
-WHERE panellist.CandidateNo=convert(left(CURRENT_USER(),INSTR(current_user(), '@')-1), decimal);
+WHERE panellist.PanellistNo= substring_index(user(), '@', 1);
+drop view PanelistView;
+
+grant select on PanelistView to "30001"@localhost;
